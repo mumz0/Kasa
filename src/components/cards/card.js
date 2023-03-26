24ca4fe61d4472b77885f60b50db17f62193e163
mumz0 <parisot.sa@gmail.com>
2023-03-26 13:39:41 +0200

    homepage done, banner and cards added

---
+++ src/components/cards/card.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link } from 'react-router-dom'
+
+function Card({ obj }) {
+  const url = `housing/${obj.id}`;
+  return (
+    <article className="card">
+      <Link to={url}>
+        <img className="card_img" src={obj.cover} alt={obj.title} />
+        <div className="card_title">
+            <h2>{obj.title}</h2>
+        </div>
+      </Link>
+    </article>
+  );
+}
+
+export default Card
